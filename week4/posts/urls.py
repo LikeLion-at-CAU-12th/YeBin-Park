2556bc750fee1dbe3edb3c99a4c99641a0e167ef
yebinnnnn <prettybird1@naver.com>
2024-03-30 19:49:03 +0900

    INIT:4주차과제생성

---
+++ week4/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from posts.views import *
+
+urlpatterns = [
+    path('', hello_world, name = 'hello_world'),
+    path('page', index, name='my-page'),
+    path('<int:id>', get_post_detail, name = "게시글 조회"),
+    path('<int:pk>/comments/', comment_create, name='comment_create')
+]
